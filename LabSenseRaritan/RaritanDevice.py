778166b7c25cb7a16d352826a79c549ea635bec9
Jason Tsao <jtsao22@gmail.com>
2013-01-28 19:41:55 -0800

    Raritan is now working with Cosm and SensorAct. Modified internal protocol to specify units when getting data. Raritan still needs clean up and better customization from config file.-a

---
+++ LabSenseRaritan/RaritanDevice.py
@@ -0,0 +1,50 @@
+import argparse                             # For parsing command line arguments
+import sys, os                              # for importing from project directory
+import time                                 # For sleeping between uploads
+
+from RaritanClient import RaritanClient 
+
+sys.path.insert(0, os.path.abspath(".."))
+from LabSenseModbusPy.common.Device import Device 
+from LabSenseModbusPy.common.Device import Device
+from LabSenseModbusPy.common.DataSinks.StdoutSink import StdoutSink
+from LabSenseModbusPy.common.DataSinks.SensorActSink import SensorActSink
+from LabSenseModbusPy.common.DataSinks.CosmSink import CosmSink 
+
+import LabSenseHandler.configReader as configReader
+
+class RaritanDevice(Device):
+
+    def __init__(self, name, IP, PORT, channels):
+        super(RaritanDevice, self).__init__()
+        self.name = name
+        self.channels = channels
+        self.devicename = "Raritan"
+        self.client = RaritanClient(name, IP, PORT)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name", help="Name of Raritan device")
+    parser.add_argument("IP", help="IP address for Raritan")
+    parser.add_argument("PORT", help="Port for Raritan")
+    parser.add_argument("time", help="Time (in seconds) between each retrieval of data from Raritan.")
+    args = parser.parse_args()
+
+    # Initialize the Raritan Device
+    device = RaritanDevice(args.name, args.IP, args.PORT,
+            configReader.config["Raritan"]["channels"])
+
+    # Create DataSinks
+    #stdoutSink = StdoutSink()
+    sensorActSink = SensorActSink()
+    cosmSink = CosmSink()
+
+    # Attach DatSinks
+    #device.attach(stdoutSink)
+    device.attach(sensorActSink)
+    device.attach(cosmSink)
+
+    sample_time = float(args.time)
+    while True:
+        data = device.getData()
+        time.sleep(sample_time)
--- LabSenseRaritan/init.sh
+++ LabSenseRaritan/init.sh
@@ -1,4 +1,5 @@
 # Usage:
 # python raritan.py [Raritan_IP] [Sample_interval]
 
-python raritan.py 172.17.5.179 20
+#python raritan.py 172.17.5.179 20
+python RaritanDevice.py NESL_Raritan 172.17.5.179 161 1
