25bdaf62fa25decd229346856d3a296cf152c0a1
Jason Tsao <jtsao22@gmail.com>
2013-03-11 19:14:18 -0700

    Removed unneccessary lua and cleaned up a bit

--- Devices/LabSenseServer/lua/motion_detected_luup_event.lua
+++ Devices/LabSenseServer/lua/motion_detected_luup_event.lua
@@ -1,13 +1,10 @@
-local time = os.time()
-local api_key = "6h8z-vpjKTBYjnt62cRh-f8yZ96SAKxoOTJjWWJkUm9uYz0g"
-local name = "NESL_MotionSensor"
-sendToLabSense("MotionSensor", name, "128.97.93.90", 8000, 0, time, api_key)
-sendToLabSense("MotionSensor", name, "128.97.93.90", 8000, 1, time + 1, api_key)
-
-sendToLabSense("MotionSensor", name, "128.97.93.190", 8000, 0, time, api_key)
-sendToLabSense("MotionSensor", name, "128.97.93.190", 8000, 1, time + 1, api_key)
-
-luup.call_delay("fallingEdge", 3)
+function risingEdge()
+    local time = os.time()
+    local api_key = "6h8z-vpjKTBYjnt62cRh-f8yZ96SAKxoOTJjWWJkUm9uYz0g"
+    local name = "NESL_MotionSensor"
+    sendToLabSense("MotionSensor", name, "128.97.93.90", 8000, 0, time, api_key)
+    sendToLabSense("MotionSensor", name, "128.97.93.90", 8000, 1, time + 1, api_key)
+end
  
 function fallingEdge()
     local time = os.time()
@@ -16,8 +13,9 @@ function fallingEdge()
     sendToLabSense("MotionSensor", name, "128.97.93.90", 8000, 1, time, api_key)
     sendToLabSense("MotionSensor", name, "128.97.93.90", 8000, 0, time + 1, api_key)
 
-    sendToLabSense("MotionSensor", name, "128.97.93.190", 8000, 1, time, api_key)
-    sendToLabSense("MotionSensor", name, "128.97.93.190", 8000, 0, time + 1, api_key)
 end
+
+risingEdge()
+luup.call_delay("fallingEdge", 3)
 
 return True
